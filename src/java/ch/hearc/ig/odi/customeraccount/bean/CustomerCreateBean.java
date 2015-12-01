151b6124744874425044905783bbc0ba14e9c647
akurtaliqi <a.kurtaliqi@gmail.com>
2015-12-01 19:45:42 +0100

    CustomerCreateBean.java prêt

---
+++ src/java/ch/hearc/ig/odi/customeraccount/bean/CustomerCreateBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.customeraccount.bean;
+
+import ch.hearc.ig.odi.customeraccount.service.Services;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author ajtene.kurtaliq
+ */
+public class CustomerCreateBean {
+    private int number;
+    private String firstName;
+    private String lastName;
+    
+    @Inject
+    Services services;
+    public CustomerCreateBean() {
+    }
+    
+    public void newCustomer () {
+        services.saveCustomer(this.number, this.firstName, this.lastName);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+    
+}
